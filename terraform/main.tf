069845bf3630b574a209974ff8885a8506e830d9
nikkee456 <159620435+nikkee456@users.noreply.github.com>
2026-01-17 19:48:43 +0530

    Restrict egress traffic to local CIDR block
    Updated egress rules to restrict outbound traffic to the local CIDR block.

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,12 +21,12 @@ resource "aws_security_group" "web_sg" {
   }
 
   egress {
-    description = "Allow outbound HTTPS traffic only"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/16"]
+}
+
 }
 
 # Secure EC2 Instance
